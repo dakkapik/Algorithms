9362d74bf56cf25c9cc80074a6ad6bc0ccc15901
Felipe Rodas <ae86rodas@outlook.com>
2022-02-01 13:42:29 -0500

    added 2 working test cases

--- jumpGame/jumpGame.js
+++ jumpGame/jumpGame.js
@@ -1,28 +1,38 @@
-const nums = [2,3,1,1,4]
-const nums2 = [2,1]
 const nums3 = [2,3,1,1,4]
 const nums4 = [1,3,2]
+
+console.log(jumpGame(nums3))
 
 function jumpGame (nums){
     let jumps = 0;
     let currentPosition = 0;
     let jumpLength = nums[currentPosition];
 
-    while(nums.length !== 1){
-        jumps ++
-        let highestJumpValueIndex = 0
+    setInterval(()=> {
+        
+        let highestJumpValueIndex = 0        
+        console.log(currentPosition)
 
         for(let i = currentPosition + jumpLength; i > currentPosition; i--){
+            console.log("jump reach: ", i + nums[i])
+            console.log(i + nums[i] === nums.length)
 
-            const finalJump = nums[i + nums[i]] === undefined || nums[i] + i === nums.length
-            if(finalJump) return jumps + 1
+            const finalJump = nums[i + nums[i]] === undefined || i + nums[i] === nums.length
+            if(finalJump){
+                return jumps + 1
+            } 
 
             if(i - currentPosition + nums[i] > i - currentPosition + nums[highestJumpValueIndex]) highestJumpValueIndex = i
 
         }
         currentPosition = highestJumpValueIndex
-        // console.log("still running")
-    }
+        jumps ++;
+    }, 500)
+
+
+    // while(nums.length !== 1){
+        
+    // }
     return jumps
 }
 
--- test/jumpGame.test.js
+++ test/jumpGame.test.js
@@ -2,23 +2,21 @@ const expect = require("chai").expect
 const jumpGame = require("../jumpGame/jumpGame")
 
 describe("jumpGame", function(){
-    
-    it("Should return the appropriate number of jumps to get to the end of the array", async function(done){
-        // // setTimeout(()=>done(), 5000)
+    this.timeout(10000)
 
-        // const result = jumpGame([2,3,1,1,4])
-        // done()
-        // expect(result).eql(2)
-        this.timeout(1000)
-        var i, test;
-        i = 0;
-        test = function() {
-        console.log(i++);
-        setTimeout(test, 1000);
-        };
-        test();
-            
-        
+    it("Given [0], Should return 0 jumps",function(){
+        const result = jumpGame([0])
+        expect(result).eql(0)
+    })
+
+    it("Given [2,1], Should return 1 jumps",function(){
+        const result = jumpGame([2,1])
+        expect(result).eql(1)
+    })
+
+    it("Given [2,3,1,1,4], Should return 2 jumps",function(){
+        const result = jumpGame([2,3,1,1,4])
+        expect(result).eql(2)
     })
     
 })
