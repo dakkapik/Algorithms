11ea61f3d1f728efb2b302e5e397b0e987e2a606
Felipe Rodas <ae86rodas@outlook.com>
2022-02-02 14:57:14 -0500

    set up basic testing

---
+++ test/subString.test.js
@@ -0,0 +1,10 @@
+const expect = require("chai").expect
+const subString = require("../lengthOfLongestSubstring/subString")
+
+describe("lengthOfLongestSubstring", function(){
+    
+    it("Should return the appropriate number of jumps to get to the end of the array", function(){
+        
+    })
+    
+})
